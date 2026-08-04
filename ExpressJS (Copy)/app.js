aa010cf1ba6b7b711751a72f274b63dd10d69604
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-08-04 20:41:29 +0530

    Update app.js

--- ExpressJS (Copy)/app.js
+++ ExpressJS (Copy)/app.js
@@ -1,24 +1,22 @@
-const http=require('http');//core module
-
 const express=require('express');//external module
 
 const requestHandler=require('./user');//local module
 
 const app=express();
 
-app.use((res, req, next)=>{
+app.use("/",(req, res, next)=>{
   console.log("FIRST MIDDLEWARE", req.url, req.method);
   next();
 });
 
-app.use((res, req, next)=>{
+app.use("/submit-details",(req, res, next)=>{
   console.log("SECOND MIDDLEWARE", req.url, req.method);
+  res.send("<p>HELLO REALITY!!!</p>");
 });
 
-const server=http.createServer(app);
 
 const PORT=4000;
 
-server.listen(PORT, ()=>{
+app.listen(PORT, ()=>{
   console.log(`SERVER RUNNING ON ADDRESS http://localhost:${PORT}`);
 });
