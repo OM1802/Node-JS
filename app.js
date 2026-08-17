479887b731ac379d78784ea173e508765d94b2de
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-08-17 18:34:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const http=require('http');
+const path=require('path');//core module
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+const express=require('express');//external module
+
+const userRouter=require('./routes/userRouter');//local module
+const hostRouter=require('./routes/hostRouter');//local module
+const rootDir=require('./utils/pathUtils');
+
+const app=express();
+
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use(express.urlencoded());
+
+app.use(userRouter);
+
+app.use("/host",hostRouter);
+
+app.use((req, res, next)=>{
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
-const port=3000;
 
-server.listen(port,() => {
-  console.log(`SERVER RUNNING ON ADDRESS http://localhost:${port}`);
+const PORT=3010;
+
+app.listen(PORT, ()=>{
+  console.log(`SERVER RUNNING ON http://localhost:${PORT}`);
 });
